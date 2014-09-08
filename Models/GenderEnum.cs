9fe759c5f7371fa419de1a592b67a62a12ea84a7
vlabor <v.labor@gmail.com>
2014-09-07 23:01:25 -0400

    Added Participant class and enum for Gender

---
+++ Models/GenderEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamSport.Models
+{
+    public enum GenderEnum
+    {
+        Male = 1,
+        Female = 2
+    }
+}
---
+++ Models/Participant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TeamSport.Models
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public GenderEnum Gender { get; set; }
+    }
+}
